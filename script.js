364698ce02910a2debb5820c460581a0033140c4
Scott Cousins <scousins1024@yahoo.com>
2024-09-30 13:17:52 -0500

    created hidden class to hide dice

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 //Selecting elements -- id by querySelector # or getElementById
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
 
+//starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
+diceEl.classList.add('hidden');
