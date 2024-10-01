26d0d97567c4803c68045ff43597f9fd8f58daf8
Scott Cousins <scousins1024@yahoo.com>
2024-10-01 06:57:41 -0500

    current score added to player 0 when rolled

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 //Selecting elements -- id by querySelector # or getElementById
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -12,6 +15,8 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -24,4 +29,11 @@ btnRoll.addEventListener('click', function () {
   diceEl.src = `dice-${dice}.png`;
 
   //3. Check for rolled 1: if true, switch player
+  if (dice !== 1) {
+    //add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; //change later
+  } else {
+    //switch to next player
+  }
 });
