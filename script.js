d13e00d1c6be71856e821046a80f04c07558bdab
Scott Cousins <scousins1024@yahoo.com>
2024-10-03 09:52:57 -0500

    function to switch player and hold global score.

--- script.js
+++ script.js
@@ -22,6 +22,14 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   //1. Generate a random dice roll
@@ -40,22 +48,19 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     //switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
   }
 });
 
 btnHold.addEventListener('click', function () {
   // 1. add current score to active players score
   scores[activePlayer] += currentScore;
-  document.getElementById(`current--${activePlayer}`).textContent =
+  document.getElementById(`score--${activePlayer}`).textContent =
     scores[activePlayer];
 
   //2. check players score is >= 100
   //Finish game
 
   //Switch to next player
+  switchPlayer();
 });
